e147415f37de26a6ec784ad402858ba69ed67b3b
dulan <77742329+dulan1376@users.noreply.github.com>
2022-03-02 03:08:43 +0800

    Create Login.php

---
+++ Login.php
@@ -0,0 +1,36 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+ <link rel="stylesheet" type="text/css" href="login.css">
+  <meta charset="UTF-8">
+  <meta name="description" content="CRUD Project">
+  <meta name="keywords" content="HTML, CSS, PHP">
+  <meta name="author" content="Dylan Thomas Matocinos">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>CRUD Project</title>
+</head> 
+<body>
+	<header>
+	
+	</header>
+	<section>
+		<nav>
+			<ul>
+				<li><a href="#">Login</a></li>
+				<li><a href="#">Sign up</a></li>
+				<li><a href="#">About</a></li>
+			</ul>
+		</nav>
+		<article>
+		
+		</article>
+	</section>
+	<footer>
+	
+	</footer>
+</body>
+</html>
