32b24c5b290a0bcaf78d2ff51fe32a1584256438
dulan <77742329+dulan1376@users.noreply.github.com>
2022-03-05 04:51:40 +0800

    Update Login.php

--- Login.php
+++ Login.php
@@ -5,7 +5,7 @@
 <!DOCTYPE html>
 <html lang="en">
  <head>
- <link rel="stylesheet" type="text/css" href="login.css">
+ <link rel="stylesheet" href="style.css"> //This can be changed depending on the name of the file
   <meta charset="UTF-8">
   <meta name="description" content="CRUD Project">
   <meta name="keywords" content="HTML, CSS, PHP">
@@ -15,18 +15,39 @@
 </head> 
 <body>
 	<header>
-	
+		<h1>LOGO</h1>
+			<nav>
+				<ul class="nav_links">
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Login</a></li>
+					<li><a href="#">Sign up</a></li>
+				</ul>
+			</nav>
+			<a class="cta" href="#"><button class = "btn">Contact</button></a>
 	</header>
 	<section>
-		<nav>
-			<ul>
-				<li><a href="#">Login</a></li>
-				<li><a href="#">Sign up</a></li>
-				<li><a href="#">About</a></li>
-			</ul>
-		</nav>
-		<article>
 		
+		<article>
+			<div class ="center">
+				<h1>Login</h1>
+				<form methos="post">
+					<div class="txt_field">
+						<input type="text" required>
+						<label>Username</label>
+						<span></span>
+					</div>
+					<div class="txt_field">
+						<input type="password" required>
+						<label>Password</label>
+						<span></span>
+					</div>
+					<div class = "pass">Forgot Password?</div>
+					<input type="submit" value="Login">
+					<div class = "signup_link">
+						Not a member? <a href="#">Signup</a>
+					</div>
+				</form>
+			</div>
 		</article>
 	</section>
 	<footer>
