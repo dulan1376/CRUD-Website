f6ea05b3a3a1bc0f11d309116b3194ce783670b4
dulan <77742329+dulan1376@users.noreply.github.com>
2022-05-12 13:46:44 +0800

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,41 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="description" content="CRUD Project">
+    <meta name="keywords" content="HTML, CSS, PHP">
+    <meta name="author" content="Dylan Thomas Matocinos">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>LAMP Project</title>
+</head>
+
+<body>
+    <header>
+        <h1>LOGO</h1>
+        <nav>
+            <ul class="nav_links">
+                <li><a href="#">Home</a></li>
+            </ul>
+        </nav>
+        <a class="cta" href="login.php"><button class="btn">Login</button></a>
+    </header>
+    <article>
+
+        <section>
+            <div class="center">
+                <h2>Signup</h2>
+
+            </div>
+        </section>
+    </article>
+    <footer>
+
+    </footer>
+</body>
+
+</html>
