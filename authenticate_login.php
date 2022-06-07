be48055eaf46b2442e0950a3b44adb98b397cda7
unikaoba <89960942+unikaoba@users.noreply.github.com>
2022-06-07 20:46:24 +0800

    Add files via upload

--- authenticate_login.php
+++ authenticate_login.php
@@ -4,20 +4,19 @@ include_once 'database_connect.php';
 
 mysqli_select_db($conn, $db);
 if (isset($_POST['login_username'])) {
-    $login_username = $_POST['login_username'];
-    $login_password = $_POST['login_password'];
-    $sql = "SELECT * FROM user_lampp_scholar WHERE user_name='" . $login_username . "'AND user_password='" . $login_password . "' ";
-
-
+    $login_username =  $conn->real_escape_string($_POST['login_username']);
+    $login_password =  $conn->real_escape_string($_POST['login_password']);
+    $sql = "SELECT * FROM user_lamp_scholar WHERE user_name= '" . $login_username . "' ";
     $result = mysqli_query($conn, $sql);
     if (mysqli_num_rows($result) > 0) {
-        $_SESSION['User'] = $_POST['login_username'];
-        if ($_POST['login_username'] == $login_username && $_POST['login_password'] == 'admin') { #This check the username and password if it matches the admin
-            header("Location: admin_homepage.php"); #For admin users
-        } else {
-            header("Location: scholar_homepage.php"); #For scholar users
+        $data = mysqli_fetch_array($result);
+        if (password_verify($login_password, $data['hashed_password'])){
+            $_SESSION['User'] = $_POST['login_username'];
+            header("Location: scholar_homepage.php");
+        }
+        else{
+            header("Wrong inputs");
         }
     } else {    #Need to apply 3 login attempts then user will wait for a 30 seconds to login again
-
     }
 }
---
+++ submit_signup.php
@@ -0,0 +1,35 @@
+<?php
+include_once 'database_connect.php';
+
+
+mysqli_select_db($conn, $db);
+if (isset($_POST['signup_email'])) {
+
+    $email = $conn->real_escape_string($_POST['signup_email']);
+    $username = $conn->real_escape_string($_POST['user_name']);
+    $password = $conn->real_escape_string ($_POST['signup_password']);
+    $confirm_password = $conn->real_escape_string($_POST['confirm_password']);
+
+
+    $sql = "SELECT * FROM user_lamp_scholar WHERE user_name='" . $username . "' AND user_email='" . $email . "' ";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) < 1) {
+
+        if ($password != $confirm_password) {
+            echo "Error in password: " . $sql . "<br>" . mysqli_error($conn);
+        } else {
+
+            $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+            $signup_token = bin2hex(openssl_random_pseudo_bytes(16));
+            $sql = "INSERT INTO user_lamp_scholar( user_name, user_email, hashed_password, unique_token) VALUES ( '$username', '$email', '$hashed_password', '$signup_token')";
+
+            if (mysqli_query($conn, $sql)) {
+                header("Location: login.php");
+            } else {
+                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+            }
+        }
+    } else {
+        echo "Error in query: " . $sql . "<br>" . mysqli_error($conn) . mysqli_num_rows($result);
+    }
+}
