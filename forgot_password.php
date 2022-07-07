bdc0daf186a481549339f9681d07e1cd8fd70cd7
G1J213 <72789455+G1J213@users.noreply.github.com>
2022-07-07 23:33:56 +0800

    Add files via upload

--- forgot_password.php
+++ forgot_password.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
     <meta charset="UTF-8">
     <meta name="description" content="CRUD Project">
     <meta name="keywords" content="HTML, CSS, PHP">
@@ -13,22 +13,20 @@
 
 <body>
     <header>
-        <h1>LOGO</h1>
-
-        <a class="btn" href="login.php">Back</a>
+        <a class="btn" href="login.php"><button>Back</button></a>
     </header>
     <article>
 
         <section>
-            <div class="center">
-                <h3>Forgot Password?</h3>
+            <div class="center_forgot">
+                <h3 class="h3">Forgot Password?</h3>
                 <form method="post" action="authenticate_email_forgot_password.php">
                     <div class="txt_field">
                         <input type="text" name="authenticate_email">
                         <label>Email</label>
                         <span></span>
                     </div>
-                    <input type="submit" value="Submit">
+                    <input type="submit" value="Send">
                 </form>
 
             </div>
