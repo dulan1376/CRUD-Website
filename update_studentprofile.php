055641ab042c23183ccf95cf13f8dad403aa5665
unikaoba <89960942+unikaoba@users.noreply.github.com>
2022-07-07 23:30:11 +0800

    Update update_studentprofile.php

--- update_studentprofile.php
+++ update_studentprofile.php
@@ -1,16 +1,16 @@
 <?php
 	include_once 'database_connect.php';
-	
+
 	mysqli_select_db($conn, $db);
 	if(isset($_POST['update'])){
+		$student_contact = $_POST['student_contact'];
+		$student_address = $_POST['student_address'];
+		$guardian_contact = $_POST['guardian_contact'];
+		$student_id = $_POST['student_id'];
 
-		$student_contact = $_POST['contact_num'];
-		$student_address = $_POST['address'];
-		$guardian_contact = $_POST['guardian'];
-
-
-		$sql = "UPDATE tbl_studentdata SET 'contact_num' = '$student_contact', 'address' = '$student_address', 'guardian' = '$guardian_contact'
-		WHERE 'student_number' = '$student_id'"; 
+		$sql = "SELECT * FROM tbl_studentdata WHERE student_id ='" . $student_id . "' ";
+		$sql = "UPDATE tbl_studentdata SET student_contact = $student_contact, student_address = $student_address, guardian_contact = $guardian_contact
+		WHERE student_id = $student_id"; 
 		mysqli_query($conn, $sql);
 
     }
