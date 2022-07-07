adda1d52458832662983cf59d663dc6e944a9601
G1J213 <72789455+G1J213@users.noreply.github.com>
2022-07-07 21:07:15 +0800

    Add files via upload

--- submit_signup.php
+++ submit_signup.php
@@ -9,7 +9,15 @@ if (isset($_POST['signup_email'])) {
     $username = $conn->real_escape_string($_POST['user_name']);
     $password = $conn->real_escape_string ($_POST['signup_password']);
     $confirm_password = $conn->real_escape_string($_POST['confirm_password']);
-
+    $fname = $conn->real_escape_string($_POST['first_name']);
+    $lname = $conn->real_escape_string($_POST['first_name']);
+    $gender = $conn->real_escape_string($_POST['gender']);
+    $student_id = $conn->real_escape_string($_POST['student_id']);
+    $scholarship = $conn->real_escape_string($_POST['scholarship']);
+    $contact_num = $conn->real_escape_string($_POST['contact_num']);
+    $guardian_num = $conn->real_escape_string($_POST['guardian_num']);
+    $address = $conn->real_escape_string($_POST['address']);
+    
 
     $sql = "SELECT * FROM user_lamp_scholar WHERE user_name='" . $username . "' AND user_email='" . $email . "' ";
     $result = mysqli_query($conn, $sql);
@@ -22,9 +30,21 @@ if (isset($_POST['signup_email'])) {
             $hashed_password = password_hash($password, PASSWORD_BCRYPT);
             $signup_token = bin2hex(openssl_random_pseudo_bytes(16));
             $sql = "INSERT INTO user_lamp_scholar( user_name, user_email, hashed_password, unique_token) VALUES ( '$username', '$email', '$hashed_password', '$signup_token')";
-
             if (mysqli_query($conn, $sql)) {
-                header("Location: login.php");
+                $sql2 = "SELECT * FROM tbl_studentdata WHERE user_name='" . $username . "' AND user_email='" . $email . "' ";
+	            $result1 = mysqli_query($conn, $sql);
+                if (mysqli_num_rows($result) > 0) {
+                    $data = mysqli_fetch_array($result1);
+                    $id = $data['id'];
+                $sql1 = "INSERT INTO tbl_studentdata( student_fname, student_lname, student_id, student_scholarship, student_gender,
+                student_contact, student_address, guardian_contact, id) VALUES ( '$fname', '$lname', '$student_id', '$scholarship', 
+                '$gender', '$contact_num', '$address', '$guardian_num', '$id')";
+                if (mysqli_query($conn, $sql1)) {
+                    header("Location: login.php");}
+                    else {
+                        echo "Error: " . $sql1 . "<br>" . mysqli_error($conn);
+                    }
+                }
             } else {
                 echo "Error: " . $sql . "<br>" . mysqli_error($conn);
             }
