d621914a7a7b3a2e0071783bd2e9923e85bd57e3
G1J213 <72789455+G1J213@users.noreply.github.com>
2022-07-11 14:57:30 +0800

    Add files via upload

--- access_studentprofile.php
+++ access_studentprofile.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-	echo $_SESSION['ID'];
 	include_once 'database_connect.php';
 	$sql = "SELECT * FROM tbl_studentdata WHERE student_id = '".$_SESSION['ID']."' ";
 	$result = mysqli_query($conn, $sql);
--- update_studentprofile.php
+++ update_studentprofile.php
@@ -1,19 +1,22 @@
 <?php
 	include_once 'database_connect.php';
 
-	echo "Hello PH";
 	mysqli_select_db($conn, $db);
 	if(isset($_POST['update'])){
-		echo "Hello World";
 		$student_contact = $_POST['student_contact'];
 		$student_address = $_POST['student_address'];
 		$guardian_contact = $_POST['guardian_contact'];
 		$student_id = $_POST['student_id'];
 
 		//$sql = "SELECT * FROM tbl_studentdata WHERE student_id ='" . $student_id . "' ";
-		$sql = "UPDATE `tbl_studentdata` SET `student_contact` = '$student_contact', `student_address` = '$student_address', `guardian_contact` = '$guardian_contact'
-		WHERE `student_id` = '$student_id'"; 
-		mysqli_query($conn, $sql);
+		$sql = "UPDATE tbl_studentdata SET student_contact = '$student_contact', student_address = '$student_address', guardian_contact = '$guardian_contact'
+		WHERE student_id = '$student_id'"; 
+		$update = mysqli_query($conn, $sql);
+		if($update){
+			mysqli_close($conn); // Close connection
+			header("location:studentprofile.php");
+			exit;
+		}
 
     }
 
